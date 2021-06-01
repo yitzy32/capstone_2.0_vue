783ecb3891c666dd18a1c8e5b899a763a911023d
Yitzy Bratt <yitzybratt32@gmail.com>
2021-05-31 20:06:15 -0400

    login

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -15,7 +17,11 @@ const routes = [
     name: "signup",
     component: Signup
   },
-
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
   {
     path: '/about',
     name: 'About',
