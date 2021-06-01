d87d1e64d7ab97318eda154b99135ed3c12c73a5
Yitzy Bratt <yitzybratt32@gmail.com>
2021-05-31 20:08:33 -0400

    logout

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Signup from "../views/Signup.vue";
 import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: "/login",
     name: "login",
     component: Login
+  },
+  {
+    path: "/logout",
+    name: "logout",
+    component: Logout
   },
   {
     path: '/about',
