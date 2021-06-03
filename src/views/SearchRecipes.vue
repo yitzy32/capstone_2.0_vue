10a0df7a2893e396261595ae1f9f654e30d1557a
Yitzy Bratt <yitzybratt32@gmail.com>
2021-06-02 21:35:09 -0400

    recipe show modal. recipe save

--- src/views/SearchRecipes.vue
+++ src/views/SearchRecipes.vue
@@ -11,8 +11,21 @@
     </form>
     <div v-for="recipe in recipes">
       {{ recipe.title }} <br>
-      <img v-bind:src="recipe.image" :height="200">
+      <img v-bind:src="recipe.image" :height="200"> <br>
+      <button v-on:click="showRecipe(recipe)">View Recipe</button> <br> <br>
     </div>
+    <dialog id="recipe-details">
+      <form method="dialog">
+        Title: {{ currentRecipe.title }} <br>
+        Prep Time: {{ currentRecipe.prep_time }} <br>
+        Servings: {{ currentRecipe.servings }} <br>
+        <a v-bind:href="currentRecipe.source_url">{{ currentRecipe.source_name }}</a> <br>
+        <img v-bind:src="currentRecipe.image" alt="" :height="200"><br>
+        Summary: {{ currentRecipe.summary }} <br>
+        <button v-on:click="saveRecipe()">Save This Recipe</button>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
@@ -27,6 +40,17 @@ export default {
       message: "Search Recipes",
       recipes: [],
       search: "",
+      currentRecipe: {
+        id: "",
+        title: "",
+        prep_time: "",
+        servings: "",
+        source_name: "",
+        source_url: "",
+        image: "",
+        summary: "",
+      },
+      id: "",
     };
   },
   created: function () {},
@@ -39,6 +63,35 @@ export default {
         console.log(response.data);
         this.recipes = response.data;
         console.log(params.search);
+      });
+    },
+    showRecipe: function (recipe) {
+      console.log("showing recipe...");
+      this.currentRecipe = recipe;
+      axios.get("/api/search_recipes/" + this.currentRecipe.id).then((response) => {
+        console.log(response.data);
+        console.log(response.data.prep_time);
+        this.currentRecipe.id = response.data.id;
+        this.currentRecipe.title = response.data.title;
+        this.currentRecipe.prep_time = response.data.prep_time;
+        this.currentRecipe.servings = response.data.servings;
+        this.currentRecipe.source_name = response.data.source_name;
+        this.currentRecipe.source_url = response.data.source_url;
+        this.currentRecipe.image = response.data.image;
+        this.currentRecipe.summary = response.data.summary;
+        this.id = response.data.id;
+        console.log("here is current recipe");
+        console.log(this.currentRecipe);
+      });
+      document.querySelector("#recipe-details").showModal();
+    },
+    saveRecipe: function () {
+      console.log("saving recipe");
+      let params = {
+        id: this.currentRecipe.id,
+      };
+      axios.post("/api/search_recipes", params).then((response) => {
+        console.log(response.data);
       });
     },
   },
