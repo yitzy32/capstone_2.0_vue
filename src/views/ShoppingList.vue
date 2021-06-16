9a40a68324917a22cc29afd1cf508ab047849fff
Yitzy Bratt <yitzybratt32@gmail.com>
2021-06-15 22:10:14 -0400

    add item to shopping list

--- src/views/ShoppingList.vue
+++ src/views/ShoppingList.vue
@@ -6,6 +6,24 @@
       {{ item.amount }}
       {{ item.unit }}
     </p>
+    <!-- <p>Name: <input type="text" v-model="newName"></p> -->
+    <p> Name
+      <select v-model="newName" id="">
+        <option v-for="ingredient in ingredients">{{ingredient.name}}</option>
+      </select>
+    </p>
+    <p> Unit
+        <select v-model="newUnit" id="">
+          <option>Cups</option>
+          <option>Tablespoons</option>
+          <option>Teaspoons</option>
+          <option>Ounces</option>
+        </select>
+      </p>
+    <p>
+      Amount: <input type="text" v-model="newAmount">
+    </p>
+    <button v-on:click="addItem">Add Item</button>
   </div>
 </template>
 
@@ -19,6 +37,10 @@ export default {
     return {
       message: "Shopping List",
       shoppingList: [],
+      ingredients: [],
+      newName: "",
+      newUnit: "",
+      newAmount: "",
     };
   },
   created: function () {
@@ -27,7 +49,24 @@ export default {
       console.log(response.data);
       this.shoppingList = response.data;
     });
+
+    axios.get("/api/ingredients").then((response) => {
+      console.log(response.data);
+      this.ingredients = response.data;
+    });
   },
-  methods: {},
+  methods: {
+    addItem: function () {
+      console.log("adding...");
+      let params = {
+        name: this.newName,
+        unit: this.newUnit,
+        amount: this.newAmount,
+      };
+      axios.post("/api/shopping_list", params).then((response) => {
+        console.log(response.data);
+      });
+    },
+  },
 };
 </script>
